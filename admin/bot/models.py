705c02c62679f68e8c75e9b33a21c9cf19b4d71c
Max3kkk <fedorovm093@gmail.com>
2022-01-11 14:32:13 +0300

    Change model relations, remove Week class, add __str__, fix data fields

--- admin/bot/models.py
+++ admin/bot/models.py
@@ -1,48 +1,78 @@
 from djongo import models
 
 
+class Group(models.Model):
+    _id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Группа: {str(self.name)}'
+
+
 class User(models.Model):
-    SEX = (('M', 'Male'), ('F', 'Female'))
-    user_id = models.IntegerField(primary_key=True)
-    telegram_id = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    second_name = models.CharField(max_length=30)
-    birth_date = models.DateField(null=True)
+    _SEX = (('M', 'Male'), ('F', 'Female'))
+    _id = models.ObjectIdField()
+    telegram_id = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=32)
+    second_name = models.CharField(max_length=32)
+    birth_date = models.DateField(blank=True)
     activated = models.BooleanField(default=False)
-    sex = models.CharField(max_length=1, choices=SEX)
+    sex = models.CharField(max_length=1, choices=_SEX)
+    groups = models.ArrayReferenceField(to=Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{str(self.second_name)} {str(self.first_name)}'
 
 
 class Exercise(models.Model):
-    exercise_id = models.PositiveIntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(default=None, blank=True)
-    link = models.URLField(max_length=250, blank=True)
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'{self.name}. {self.description}.'
+
+    class Meta:
+        abstract = True
 
 
 class Gymnastic(models.Model):
-    gymnastic_id = models.IntegerField(primary_key=True)
-    exercise = models.EmbeddedField(model_container=Exercise)
-    value = models.CharField(max_length=50)
+    _id = models.ObjectIdField()
+    exercise = models.ArrayReferenceField(to=Exercise, on_delete=models.CASCADE)
+    value = models.CharField(
+        max_length=50,
+    )
+
+    def __str__(self):
+        return f'{self.exercise.name}. {self.exercise.description}. {self.value}'
+
+    class Meta:
+        abstract = True
 
 
 class Set(models.Model):
-    set_id = models.IntegerField(primary_key=True)
+    _id = models.ObjectIdField()
     description = models.CharField(max_length=50)
     rounds_amount = models.PositiveIntegerField()
     gymnastics = models.ArrayField(model_container=Gymnastic)
 
+    def __str__(self):
+        return f'{self.description}. Кол-во раундов: {self.rounds_amount}'
+
+    class Meta:
+        abstract = True
+
 
 class Training(models.Model):
-    training_id = models.IntegerField(primary_key=True)
+    _id = models.ObjectIdField()
     description = models.TextField(default=None, blank=True)
-    training_type = models.CharField(max_length=40)
-    index = models.PositiveIntegerField()
+    groups = models.ArrayReferenceField(to=Group, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
     week_start_date = models.DateField()
     min_rm_percent = models.PositiveIntegerField()
     max_rm_percent = models.PositiveIntegerField()
 
-
-class Week(models.Model):
-    week_id = models.IntegerField(primary_key=True)
-    week_start_date = models.DateField()
-    trainings = models.ArrayField(model_container=Training)
+    def __str__(self):
+        return f'Неделя {self.week_start_date}. Тренировка № {self.order}. {self.description}'
