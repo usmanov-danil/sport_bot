c9f4b2964412b7034e9c4ee2fe2c9ac521684d0c
usmanov-danil <usmanovdanilr@gmail.com>
2022-01-15 16:17:36 +0300

    Refactor models and admin panel cleaning

--- admin/bot/admin.py
+++ admin/bot/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from .models import Exercise, Group, Gymnastic, Set, Training, User
 
-admin.site.register(User)
-admin.site.register(Training)
-admin.site.register(Group)
-admin.site.register(Set)
-admin.site.register(Exercise)
-admin.site.register(Gymnastic)
+
+class AdminSite(admin.AdminSite):
+    site_title = "Спорт бот | Панель администратора"
+    site_header = "Панель администратора"
+    index_title = "Спорт бот | Панель администратора"
+
+
+admin_site = AdminSite()
+
+
+admin_site.register(Group)
+admin_site.register(User)
+admin_site.register(Exercise)
+admin_site.register(Gymnastic)
+admin_site.register(Set)
+admin_site.register(Training)
--- admin/bot/models.py
+++ admin/bot/models.py
@@ -1,65 +1,108 @@
+import uuid
+from tokenize import group
+
 from djongo import models
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
 
     def __str__(self):
         return f'Группа: {str(self.name)}'
 
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
 
 class User(models.Model):
-    _SEX = (('M', 'Male'), ('F', 'Female'))
+    _SEX = (('M', 'Мужской'), ('F', 'Женский'))
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     telegram_id = models.CharField(max_length=32)
-    first_name = models.CharField(max_length=32)
-    second_name = models.CharField(max_length=32)
-    birth_date = models.DateField(blank=True)
-    activated = models.BooleanField(default=False)
-    sex = models.CharField(max_length=1, choices=_SEX)
-    groups = models.ArrayReferenceField(to=Group, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32, verbose_name='Имя')
+    second_name = models.CharField(max_length=32, verbose_name='Фамилия')
+    birth_date = models.DateField(blank=True, verbose_name='Дата рождения')
+    activated = models.BooleanField(default=False, verbose_name='Активированный')
+    sex = models.CharField(max_length=1, choices=_SEX, verbose_name='Пол', blank=True, null=True)
+    groups = models.ArrayReferenceField(
+        to=Group, on_delete=models.SET_NULL, null=True, verbose_name='Группы'
+    )
 
     def __str__(self):
-        return f'{str(self.second_name)} {str(self.first_name)}'
+        return f'{str(self.first_name)} {str(self.second_name)}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(default=None, blank=True)
-    link = models.URLField(blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(default=None, blank=True, verbose_name='Описание')
+    link = models.URLField(blank=True, verbose_name='Ссылка на видео')
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Упражнение'
+        verbose_name_plural = 'Упражнения'
 
 
 class Gymnastic(models.Model):
-    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    description = models.TextField(blank=True)
-    value = models.CharField(max_length=50, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, verbose_name='Упражнение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    value = models.CharField(max_length=50, blank=False, verbose_name='Повторения')
 
     def __str__(self):
-        return f'{self.exercise.name} {self.exercise.description} {self.description}. {self.value}'
+        return f'{self.exercise.name}: {self.value} повторов'
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
 
 
 class Set(models.Model):
-    description = models.TextField(blank=True)
-    rounds_amount = models.PositiveIntegerField()
-    gymnastics = models.ArrayReferenceField(to=Gymnastic, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField(blank=True, verbose_name='Описание')
+    rounds_amount = models.PositiveIntegerField(verbose_name='Кол-во раундов')
+    gymnastics = models.ArrayReferenceField(
+        to=Gymnastic, on_delete=models.CASCADE, verbose_name='Задания'
+    )
 
     def __str__(self):
-        return (
-            f' Кол-во раундов: {self.rounds_amount} {self.description} Названия упражнений: <TODO>'
+        ex_names = list(
+            Set.objects.filter(id=self.id).values_list('gymnastics__exercise__name', flat=True)
         )
+        return f'Упр: {"/".join(ex_names)} Кол-во раундов: {self.rounds_amount}'
+
+    class Meta:
+        verbose_name = 'Сет'
+        verbose_name_plural = 'Сеты'
 
 
 class Training(models.Model):
-    description = models.TextField(default=None, blank=True)
-    groups = models.ArrayReferenceField(to=Group, on_delete=models.CASCADE)
-    sets = models.ArrayReferenceField(to=Set, on_delete=models.CASCADE)
-    order = models.PositiveIntegerField(default=3)
-    week_start_date = models.DateField()
-    min_rm_percent = models.PositiveIntegerField()
-    max_rm_percent = models.PositiveIntegerField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField(default=None, blank=True, verbose_name='Описание')
+    groups = models.ArrayReferenceField(
+        to=Group, on_delete=models.SET_NULL, verbose_name='Группы', null=True
+    )
+    sets = models.ArrayReferenceField(to=Set, on_delete=models.CASCADE, verbose_name='Сеты')
+    order = models.PositiveIntegerField(default=3, verbose_name='Порядок')
+    week_start_date = models.DateField(verbose_name='Дата начала недели')
+    min_rm_percent = models.PositiveIntegerField(verbose_name='Мин. процент от макс. веса')
+    max_rm_percent = models.PositiveIntegerField(verbose_name='Макс. процент от макс. веса')
 
     def __str__(self):
-        return f'Неделя {self.week_start_date}. Тренировка № {self.order}. {self.description} Группы: <TODO>'
+        group_names = list(
+            Training.objects.filter(id=self.id).values_list('groups__name', flat=True)
+        )
+        return f'Неделя {self.week_start_date}. Тренировка № {self.order}. Группы: {", ".join(group_names)}'
+
+    class Meta:
+        verbose_name = 'Тренировка'
+        verbose_name_plural = 'Тренировки'
